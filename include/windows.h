6c0cbf3da9ac24d8f4e03229ec48e7cd2f1b9e4f
valentin.lattay <valentin.lattay@epitech.eu>
2018-05-29 06:10:07 +0200

    dim_t type for dimensions contains width and height

---
+++ include/windows.h
@@ -0,0 +1,13 @@
+/*
+** RUDREAIS, 05/27/18
+** roguelike
+** File description:
+** main
+*/
+
+#pragma once
+
+typedef struct {
+    int width;
+    int height;
+} dim_t;
---
+++ src/windows/create.c
@@ -0,0 +1,14 @@
+/*
+** RUDREAIS, 05/27/18
+** roguelike
+** File description:
+** map generator header
+*/
+
+#include <ncurses.h>
+#include <stdlib.h>
+
+WINDOW *create_window(cursor_t *pos, dim_t *size)
+{
+
+}
