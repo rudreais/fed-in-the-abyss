956150aa282b648ed0f16b6d7806ba9b3a4681ae
valentin.lattay <valentin.lattay@epitech.eu>
2018-06-07 18:49:48 +0200

    deleted structures to rewrite them

--- include/board.h
+++ include/board.h
@@ -19,8 +19,17 @@
 #define START_FLOOR '>'
 
 // are used to determine the collision
+// found in src/map/ as vars.c
 extern const char hard_tiles[];
 extern const char transparent_tiles[];
+
+/**
+ * @param map_t *map
+ * @return void
+ * @purpose fulfill map pointer with the read file
+ * TEMP FUNCTION AS THE MAP WILL BE RANDOMLY GENERATED
+ */
+void get_map(map_t *map);
 
 /**
  * structure to easier use positions
@@ -39,57 +48,3 @@ typedef struct {
     int width;
     int height;
 } dim_t;
-
-typedef struct {
-    int str;
-    int intel;
-    int wis;
-    int dex;
-    int con;
-    int chr;
-} stats_t;
-
-typedef struct {
-    char *name;
-    char appear;
-    stats_t characteristics;
-    cursor_t *pos;
-} character_t;
-
-/**
- * the terminal is a block composed of "bricks" also called tabs
- * each tabs has its own WINDOW (ncurses), size, and position.
- * for example the main window (where the map is) is a tab
- */
-
-typedef struct {
-    WINDOW *win;    
-    cursor_t *tab_pos;
-    dim_t *tab_size;
-} tab_t;
-
-typedef struct {
-    char **map;
-	int ymax;
-    tab_t *parent_tab;
-    cursor_t *pos;
-} map_t;
-
-/**
- * each floor of the dungeon will has a certain design randomly generated
- * the structure contains the number of line the map contains (reading purpose)
- */
-
-typedef struct {
-    int ymax;
-    char **design;
-    character_t **chars;
-} floor_t;
-
-/**
- * structure that contains every floors, character, etc
- */
-
-typedef struct {
-    floor_t **floors;
-} dungeon_t;
