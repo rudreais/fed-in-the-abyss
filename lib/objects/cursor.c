e45931ad3f69aa8273edd9d8850407c323673b5c
rudreais <valentinlattay@gmail.com>
2018-07-10 13:30:58 +0200

    cursor lib

---
+++ lib/objects/cursor.c
@@ -0,0 +1,29 @@
+/*
+ * Filename: /home/rudreais/prog/fed-in-the-abyss/src/cursor.c
+ * Path: /home/rudreais/prog/fed-in-the-abyss/src
+ * Created Date: Tuesday, July 10th 2018, 1:17:36 pm
+ * Author: rudreais
+ * 
+ * Copyright (c) 2018 Your Company
+ */
+
+#include <stdlib.h>
+#include "cursor.h"
+
+void cursor_modify(Cursor *self, int newx, int newy)
+{
+    self->x = newx;
+    self->y = newy;
+}
+
+void CursorDestroy(Cursor *self)
+{
+}
+
+void CursorInit(Cursor *self, int x, int y)
+{
+    self->x = x;
+    self->y = y;
+    self->destroy = CursorDestroy;
+    self->modify = cursor_modify;
+}
