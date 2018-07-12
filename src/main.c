e4094495e57bec52b28015317766380fc929db3f
rudreais <valentinlattay@gmail.com>
2018-07-12 02:19:17 +0200

    removing tmp files & adding sources for maps lib

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
  **
  */
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
@@ -11,17 +12,37 @@
 #include "cursor.h"
 #include "fita.h"
 #include "path.h"
+#include "maps.h"
 
 void init_curses(void)
 {
-    initscr();
-    raw();
-    keypad(stdscr, TRUE);
-    noecho();
+	initscr();
+	raw();
+	keypad(stdscr, TRUE);
+	noecho();
+}
+
+void loop(void)
+{
+	WINDOW *win = newwin(30, 70, 5, 5);
+	Cursor cam_ini;
+	files_t *files = malloc(sizeof(files_t));
+
+	files_init(files, getpath("maps"));
+	printw("%d\n", files->size);
+	cam_ini.init = CursorInit;
+	cam_ini.init(&cam_ini, 1, 1); // starting at the very top left
+
+	box(win, 0, 0);
+	refresh();
+	wrefresh(win);
+	getch();
 }
 
 int main(void)
 {
-    printf("%s\n", getpath("maps"));
-    return 0;
+	init_curses();
+	loop();
+	endwin();
+	return 0;
 }
