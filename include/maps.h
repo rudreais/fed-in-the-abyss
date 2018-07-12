87f2aadd8d784e86e3cd124d6004bf34c025e915
rudreais <valentinlattay@gmail.com>
2018-07-12 02:17:50 +0200

    forgotten header for maps lib lel

---
+++ include/maps.h
@@ -0,0 +1,16 @@
+#pragma once
+
+typedef struct file_s file_t;
+typedef struct files_s files_t;
+
+struct file_s {
+	char **map;
+	int y_len;
+};
+
+struct files_s {
+	file_t **files;
+	int size;
+};
+
+void files_init(files_t *files, const char *path_to_dir);
