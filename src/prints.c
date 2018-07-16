c698b4f2139bee2579c10818e6d7d55fc20e33e2
rudreais <valentinlattay@gmail.com>
2018-07-16 15:31:28 +0200

    (this is the actual print file)

---
+++ src/prints.c
@@ -0,0 +1,36 @@
+//
+// Created by rudreais on 16/07/18.
+//
+
+#include <ncurses.h>
+#include "files.h"
+
+void print_char(char c)
+{
+	start_color();
+	init_pair(1, COLOR_BLUE, COLOR_BLACK);
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);
+	if (c == '~') {
+		attron(COLOR_PAIR(1));
+		printw("%c", c);
+		attroff(COLOR_PAIR(1));
+	} else if (c == '.') {
+		attron(COLOR_PAIR(2));
+		printw("%c", c);
+		attroff(COLOR_PAIR(2));
+	} else if (c == '%') {
+		attron(COLOR_PAIR(3));
+		printw("%c", c);
+		attroff(COLOR_PAIR(3));
+	}
+}
+
+void print_map(WINDOW *win, files_t *maps)
+{
+	for (int i = 0; i < 55; i++) {
+		for (int j = 0; j < 160; j++)
+			print_char(maps->files[0]->map[i][j]);
+		printw("\n");
+	}
+}
