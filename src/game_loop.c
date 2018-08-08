8d81ac392410d06abc532fc61a41c5bbb309eb6a
Pacôme Françon <pacome.francon@epitech.eu>
2018-08-08 19:10:08 +0200

    1. split game_loop() in sub-function => camera()
    - solve indentation problems
    - add enum direction, for readability/clean code

--- src/game_loop.c
+++ src/game_loop.c
@@ -5,15 +5,12 @@
 void game_loop(map_t **maps, char **old_state)
 {
 	WINDOW *win = newwin(GET_HEIGHT, GET_WIDTH, 0, 0);
-	cursor_t fixed;
 	player_t player;
 	enemy_t *enemies[10];
 	int c = 0; // key pressed
-	int border = 0; // define if fixed cam pos is used or not
 	cursor_t enemy_pos;
 
 	create_player(&player);
-	fixed = player.pos;
 	add_enemy(enemies);
 	while (c != 'q') {
 		enemy_pos = move_charac(c, &player.pos, &player.pos_bak, maps[0]->map);
@@ -29,18 +26,7 @@ void game_loop(map_t **maps, char **old_state)
 		assign_player(maps[0]->map, old_state, &player.pos, &player.pos_bak);
 		if (enemy_pos.x != -1 && enemy_pos.y != -1)
 			attack(enemies, &enemy_pos, &player, &player);
-		if ((border = border_cam(&player.pos)) > 0) {
-			if (border == 2 || border == 4) // border on left/right
-				fixed.y = player.pos.y;
-			if (border == 1 || border == 3) // border on top/bottom
-				fixed.x = player.pos.x;
-			centered_map(win, &fixed, maps);
-			refresh();
-			wrefresh(win);
-		} else {
-			fixed = player.pos_bak;
-			centered_map(win, &player.pos, maps);
-		}
+		camera(&player, win, maps);
 		wmove(win, 1, 1); // test purpose
 		wprintw(win, "%d", enemies[0]->charac.hp);
 		wprintw(win, "\t%d\t%d", enemy_pos.x, enemy_pos.y);
